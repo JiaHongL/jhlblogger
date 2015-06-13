ca5ddfe47b913e44912cd7f9b6b25ea10b818750
JiaHongL <jiahongl@114-38-157-200.dynamic.hinet.net>
2015-06-13 21:43:47 +0800

    fix bug

--- app/js/about.js
+++ app/js/about.js
@@ -8,4 +8,11 @@ myApp.controller('aboutCtrl', ['$scope','blogger','$rootScope',function ($scope,
 		 $scope.intro_me = res.data.results[0].intro;
 		 $scope.intro_version = res.data.results[0].version;
 	});
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  ga('create', 'UA-64071861-1', 'auto');
+  ga('send', 'pageview');
+  ga('require', 'displayfeatures');
 }]);
