8dab8cd05236d3085bc23e090965b93335ce3caf
JiaHongL <jiahongl@114-38-157-200.dynamic.hinet.net>
2015-06-13 21:32:48 +0800

    fix bug

---
+++ app/js/message.js
@@ -0,0 +1,7 @@
+myApp.controller('messageCtrl', ['$scope','blogger','$rootScope',function ($scope,blogger,$rootScope) {
+// blogger.get_all_article().then(function(res){
+	console.log('TEST');
+// 	$scope.article_list = res.data.results;
+// });
+
+}]);
